7fb25a57ce795d79b53bbdd50ac2451f53956e09
polbb <paulbessone@gmail.com>
2024-02-21 10:14:37 +0000

    css ubuntu

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -19,7 +19,7 @@ from database import *
 
 # # BELOW IS FOR STREAMLIT DEPLOYMENT
 # client = OpenAI(api_key=st.secrets.openai)
-with open( "style.css" ) as css:
+with open( "src/scripts/style.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 
 # st. set_page_config(layout="wide")
@@ -32,7 +32,6 @@ col1, col2, col3, c4, c5, c6, c7 ,c8 = st.columns([3,3,1,1,1,1,1,1])
 company_number = col1.text_input("Enter the company number")
 iterations = col2.number_input("How many review vectors?", value=1)
 
-# add a sum function here that sums two numbers
 
 
 calculate_ratio = st.button("Start Analysis")
